848340abf928dd518f59bf1b56a0d1ddd1c6015c
Dikontay <dalimbayeva@icloud.com>
2023-05-09 14:32:41 +0600

    _-_

--- MyBinarySearchTree/src/BinarySearchTree.java
+++ MyBinarySearchTree/src/BinarySearchTree.java
@@ -1,34 +1,54 @@
-public class BinarySearchTree <T extends Comparable<T>>{
-    private static class MyNode<T extends Comparable<T>>{
-        T data;
-        MyNode<T> left;
-        MyNode<T> right;
-
-        int size;
-        public MyNode(T data,int size){
-            this.data = data;
-            this.size = size;
+public class BinarySearchTree <K extends Comparable<K>, V>{
+    private Node root;
+   private class Node{
+        private K key;
+        private V val;
+        private Node left, right;
+        public int size = 1;
+        public Node(K key, V value){
+            this.key = key;
+            this.val = value;
         }
-    }
-    MyNode <T> root ;
-    private int size;
-    public  boolean isEmpty(){
-        return root == null;
-    }
-    public void put(T value){
-        root = insertRecursive(value, root);
-    }
-    private MyNode<T> insertRecursive(T value, MyNode<T>  current){
-        if(current == null){
-            return new MyNode<>(value, 1);
-        }
-        if(current.data.compareTo(value)>0){
-            current.right= insertRecursive(value, current.right);
-        }else  current.left= insertRecursive(value, current.left);
-        current.size= current.left.size+current.right.size+1;
-        return current;
-    }
+   }
 
-    public V get()
+   public void put(K key, V val){
+        root=put(root, key,val);
+   }
+   private Node put(Node node, K key, V val){
+       if(node == null){
+           return new Node(key, val);
+       }
+       if(key.compareTo(node.key)>0){
+           node.right=put(node.right, key, val);
+       } else node.left= put(node.left, key, val);
 
+
+
+      node.size= isNotNull(node.left)+isNotNull(node.right)+1 ;
+      return node;
+   }
+   private int isNotNull(Node n){
+       return n!=null? 1 : 0;
+   }
+   public V get(K key){
+
+       return find(root, key);
+   }
+   private V find(Node n, K key){
+       if(n==null){
+           return null;
+       }
+       if(n.key.compareTo(key)==0){
+           return n.val;
+       }
+       if(n.key.compareTo(key)>0){
+          return find(n.right, key);
+       }else return find(n.left, key);
+   }
+   public void delete(K key){
+
+   }
+   public Iterable<K> iterator(){
+       return null;
+   }
 }
--- MyBinarySearchTree/src/Main.java
+++ MyBinarySearchTree/src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-      
+      BinarySearchTree<Integer, String> bst = new BinarySearchTree<>();
+      bst.put(3, "Yernazar");
+      bst.put(7, "Yernazar");
+      bst.put(5, "Yernazar");
+      bst.put(8, "Yernazar");
+      bst.put(9, "Yernazar");
+      bst.put(10, "Yernazar");
+      bst.put(2, "Yernazar");
+      bst.put(1, "Yernazar");
+      bst.put(4, "Yernazar");
+      System.out.println(bst.get(1));
+
 
     }
 }
